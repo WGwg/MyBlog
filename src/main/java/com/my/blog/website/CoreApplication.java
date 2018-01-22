a60df6562ed2946eeebabb7384779d4106f70065
WGwg <956910931@qq.com>
2018-01-22 17:52:33 +0800

    提交云服务器

--- src/main/java/com/my/blog/website/CoreApplication.java
+++ src/main/java/com/my/blog/website/CoreApplication.java
@@ -6,7 +6,9 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -18,7 +20,7 @@ import javax.sql.DataSource;
 @MapperScan("com.my.blog.website.dao")
 @SpringBootApplication
 @EnableTransactionManagement
-public class CoreApplication {
+public class CoreApplication extends SpringBootServletInitializer {
     @Bean(initMethod = "init", destroyMethod = "close")
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
@@ -38,7 +40,6 @@ public class CoreApplication {
     public PlatformTransactionManager transactionManager() {
         return new DataSourceTransactionManager(dataSource());
     }
-
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
